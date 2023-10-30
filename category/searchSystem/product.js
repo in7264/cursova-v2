494e7bf74fffaca0408b1e3dc33763eb60ed15b5
Ivan Nimych <in7264@gmail.com>
2023-10-30 09:53:57 +0200

    1.10v

--- category/searchSystem/product.js
+++ category/searchSystem/product.js
@@ -26,32 +26,34 @@
         function initializeEventHandlers() {
             const buyButtons = document.querySelectorAll('.buy-button');
             const cartItems = document.getElementById('cart-items');
-
+        
             buyButtons.forEach(button => {
                 button.addEventListener('click', () => {
                     const productName = button.getAttribute('data-product');
                     const productImage = button.getAttribute('data-image');
                     const productPrice = button.getAttribute('data-price');
-                    const cartItem = document.createElement('li');
+                    const cartItem = document.createElement('div');
+                    cartItem.className = 'cart-item'; // Додайте клас cart-item
                     cartItem.innerHTML = `
                         <img src="${productImage}" alt="${productName}">
                         <span>${productName}</span>
                         <span class="price">${productPrice} грн</span>
                     `;
                     cartItems.appendChild(cartItem);
-
+        
                     // Зберігаємо стан корзини в localStorage
                     const cart = JSON.parse(localStorage.getItem('cart')) || [];
                     cart.push({ name: productName, image: productImage, price: productPrice });
                     localStorage.setItem('cart', JSON.stringify(cart));
                 });
             });
-
+        
             // Відображення збереженого вмісту корзини при завантаженні сторінки
             const savedCart = JSON.parse(localStorage.getItem('cart'));
             if (savedCart) {
                 savedCart.forEach(product => {
-                    const cartItem = document.createElement('li');
+                    const cartItem = document.createElement('div');
+                    cartItem.className = 'cart-item';
                     cartItem.innerHTML = `
                         <img src="${product.image}" alt="${product.name}">
                         <span>${product.name}</span>
@@ -61,3 +63,4 @@
                 });
             }
         }
+        
